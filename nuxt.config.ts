a6e0e51335468f0dd7f4e4265d3ab963ce74caca
Chananchida Srithongdee <saiparn5589@gmail.com>
2024-06-21 12:05:24 +0700

    chore: change app icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1',
+			title: "Swensen's",
+		},
+	},
+
 	devtools: { enabled: true },
+
 	css: ['~/assets/css/main.css', '~/assets/css/en.css'],
 
 	postcss: {
